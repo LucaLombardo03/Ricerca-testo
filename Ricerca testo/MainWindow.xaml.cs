a8296c87d36a3afdff44c3266f752ac63e54a262
luca lombardo <lucalombardo03@gmail.com>
2020-03-10 19:16:20 +0100

    Add project files.

---
+++ Ricerca testo/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ricerca_testo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btncerca_Click(object sender, RoutedEventArgs e)
+        {
+            string testoiniziale = txttestoiniziale.Text;
+            string testodacercare = txttestocercare.Text;
+            bool esiste = testoiniziale.Contains(testodacercare);
+            if (esiste == true)
+            {
+                
+            }
+            else
+                lblfine.Content = (false);
+        }
+    }
+}
